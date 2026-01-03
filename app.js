0d52bf90d15b4eb9f9373224b5772b04d7156927
Atelier Electronique Medenine <aemedenine@gmail.com>
2026-01-03 14:18:47 +0100

    Update app.js

--- app.js
+++ app.js
@@ -179,6 +179,7 @@ document.addEventListener('DOMContentLoaded', () => {
     startNewsRotation();
     updateTime();
     initFAQ();
+    updateEqualizerVisibility();
   }
 
   toggleBtn.addEventListener('click', () => {

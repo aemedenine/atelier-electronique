ab005f85f42e3a28610d8c4f48c6b542b97a2ac6
Atelier Electronique Medenine <aemedenine@gmail.com>
2026-02-28 15:18:27 +0100

    Update and rename api-football.php to api-sofascore.php

--- api-sofascore.php
+++ api-sofascore.php
@@ -1,16 +1,15 @@
 <?php
-// api-football.php
 header('Content-Type: application/json');
 
-// هنا تحط مفتاحك و host من RapidAPI
 $apiKey = '7b51fa7498mshc0f1433aaed4c9fp1d3febjsn9f377995c660';
-$host   = 'v3.football.api-sports.io';
+$host   = 'sofascore.p.rapidapi.com';
 
-// Endpoint من URL: ?endpoint=standings أو fixtures&params=...
-$endpoint = $_GET['endpoint'] ?? '';
-$params   = $_GET['params'] ?? '';
+// endpoint: ?tournamentId=17&seasonId=29415&pageIndex=0
+$tournamentId = $_GET['tournamentId'] ?? 17;
+$seasonId     = $_GET['seasonId'] ?? 29415;
+$pageIndex    = $_GET['pageIndex'] ?? 0;
 
-$url = "https://$host/$endpoint?$params";
+$url = "https://$host/tournaments/get-next-matches?tournamentId=$tournamentId&seasonId=$seasonId&pageIndex=$pageIndex";
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
@@ -21,6 +20,7 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, [
 ]);
 
 $response = curl_exec($ch);
+
 if(curl_errno($ch)) {
     echo json_encode(['error' => curl_error($ch)]);
     exit;
